10494f67937fe24c83eca22574543f2b39d0a600
Rafael Souza <faelsos98@gmail.com>
2022-09-12 19:12:25 -0300

    first commit

---
+++ bd.py
@@ -0,0 +1,51 @@
+
+cars = [
+    {
+        "id": 1,
+        "marca": "Ford",
+        "modelo": "Focus",
+        "ano" : 2005
+    },
+    {
+        "id": 2,
+        "marca": "Chevrolet",
+        "modelo": "Camaro",
+        "ano" : 2013
+    },
+    {
+        "id": 3,
+        "marca": "Hyundai",
+        "modelo": "HB20",
+        "ano" : 2020
+    },
+    {
+        "id": 4,
+        "marca": "Ford",
+        "modelo": "Focus",
+        "ano" : 2005
+    },
+    {
+        "id": 5,
+        "marca": "Volksvagem",
+        "modelo": "Voyage",
+        "ano" : 2010
+    },
+    {
+        "id": 6,
+        "marca": "Citroen",
+        "modelo": "C4 Pallas",
+        "ano" : 2017
+    },
+    {
+        "id": 7,
+        "marca": "Fiat",
+        "modelo": "Toro",
+        "ano" : 2021
+    },
+    {
+        "id": 8,
+        "marca": "Mercedes",
+        "modelo": "A180",
+        "ano" : 2019
+    },
+]
---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, make_response, jsonify, request
+from bd import cars
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.route("/cars", methods=['GET', 'POST'])
+def cars_methods():
+    if request.method == 'GET':
+        return make_response( jsonify(
+                message='Cars list',
+                data= cars
+            ))
+    elif request.method == 'POST':
+        car = request.json
+        cars.append(car)
+        return make_response(
+            jsonify(
+                message='Car registered!',
+                data=car
+            )
+        )
+
+@app.route("/cars/del", methods=['DELETE'])
+def delete_car():
+    return make_response(
+        jsonify(
+            cars.pop()
+        )
+    )
+   
+app.run()
